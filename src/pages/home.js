77222273d635f6131c2806b9face083b6c018785
madelineponiman <meddonaldss@gmail.com>
2023-10-24 00:31:58 +1100

    add navbar and home page

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import './home.css';
+ 
+const About = () => {
+    return (
+        <div className="hero">
+            <div className="home-box">
+                <h1 className='text-white text-7xl'>MADELINE PONIMAN</h1>
+                <h2 className='text-white text-5xl font-medium'>FRONTEND DEVELOPER</h2>
+            </div>
+        </div>
+    );
+};
+ 
+export default About;
